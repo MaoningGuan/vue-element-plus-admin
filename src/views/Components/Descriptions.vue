a896746c11f84bfc6babe2ec21fa71f175bc889d
gmn <1812711281@qq.com>
2025-02-16 13:38:37 +0800

    feat: 支持信息展示页面

--- src/views/Components/Descriptions.vue
+++ src/views/Components/Descriptions.vue
@@ -1,9 +1,9 @@
 <script setup lang="tsx">
 import { Descriptions } from '@/components/Descriptions'
 import { useI18n } from '@/hooks/web/useI18n'
-import { reactive } from 'vue'
-import { Form } from '@/components/Form'
-import { ElFormItem, ElInput } from 'element-plus'
+import { ref, reactive } from 'vue'
+import { Form, FormSchema } from '@/components/Form'
+import { Dialog } from '@/components/Dialog'
 import { useValidator } from '@/hooks/web/useValidator'
 import { useForm } from '@/hooks/web/useForm'
 import { DescriptionsSchema } from '@/components/Descriptions'
@@ -11,6 +11,26 @@ import { DescriptionsSchema } from '@/components/Descriptions'
 const { required } = useValidator()
 
 const { t } = useI18n()
+
+const dialogVisible2 = ref(false)
+
+// 页面mount的时候调用fetchData来获取数据显示
+// 模拟API请求
+// const fetchData = () => {
+//   setTimeout(() => {
+//     detailData.value = [
+//       { label: '创建时间', value: '2023-10-01 08:00:00' },
+//       { label: '状态', value: '已发布', type: 'tag', tagType: 'success' },
+//       { label: '文档链接', value: 'https://vue-element-plus-admin.com', type: 'link' },
+//       { label: '描述', value: '基于Vue3 + Element Plus的后台管理系统模板' }
+//     ]
+//     loading.value = false
+//   }, 1000)
+// }
+
+// onMounted(() => {
+//   fetchData()
+// })
 
 const data = reactive({
   username: 'chenkl',
@@ -47,97 +67,42 @@ const schema = reactive<DescriptionsSchema[]>([
   }
 ])
 
-const schema2 = reactive<DescriptionsSchema[]>([
+const schema2 = reactive<FormSchema[]>([
   {
     field: 'username',
     label: t('descriptionsDemo.username'),
-    slots: {
-      label: (row) => {
-        return <span class="is-required--item">{row.label}</span>
-      },
-      default: () => {
-        return (
-          <ElFormItem prop="username">
-            <ElInput v-model={form.username} />
-          </ElFormItem>
-        )
-      }
-    }
+    component: 'Input',
+    value: data.username
   },
   {
     field: 'nickName',
     label: t('descriptionsDemo.nickName'),
-    slots: {
-      label: (row) => {
-        return <span class="is-required--item">{row.label}</span>
-      },
-      default: () => {
-        return (
-          <ElFormItem prop="nickName">
-            <ElInput v-model={form.nickName} />
-          </ElFormItem>
-        )
-      }
-    }
+    component: 'Input',
+    value: data.nickName
   },
   {
     field: 'phone',
     label: t('descriptionsDemo.phone'),
-    slots: {
-      label: (row) => {
-        return <span class="is-required--item">{row.label}</span>
-      },
-      default: () => {
-        return (
-          <ElFormItem prop="phone">
-            <ElInput v-model={form.phone} />
-          </ElFormItem>
-        )
-      }
-    }
+    component: 'Input',
+    value: data.phone
   },
   {
     field: 'email',
     label: t('descriptionsDemo.email'),
-    slots: {
-      label: (row) => {
-        return <span class="is-required--item">{row.label}</span>
-      },
-      default: () => {
-        return (
-          <ElFormItem prop="email">
-            <ElInput v-model={form.email} />
-          </ElFormItem>
-        )
-      }
-    }
+    component: 'Input',
+    value: data.email
   },
   {
     field: 'addr',
     label: t('descriptionsDemo.addr'),
-    slots: {
-      label: (row) => {
-        return <span class="is-required--item">{row.label}</span>
-      },
-      default: () => {
-        return (
-          <ElFormItem prop="addr">
-            <ElInput v-model={form.addr} />
-          </ElFormItem>
-        )
-      }
+    component: 'Input',
+    componentProps: {
+      type: 'textarea',
+      rows: 2
     },
-    span: 24
+    value: data.addr
   }
 ])
-
-const form = reactive({
-  username: '',
-  nickName: '',
-  phone: '',
-  email: '',
-  addr: ''
-})
 
 const rules = reactive({
   username: [required()],
@@ -148,35 +113,44 @@ const rules = reactive({
 })
 
 const { formRegister, formMethods } = useForm()
-const { getElFormExpose } = formMethods
+const { getElFormExpose, getFormData } = formMethods
 
-const formValidation = async () => {
-  const elFormExpose = await getElFormExpose()
-  elFormExpose?.validate((isValid) => {
-    console.log(isValid)
+const formSubmit = async () => {
+  const elForm = await getElFormExpose()
+  const valid = await elForm?.validate().catch((err) => {
+    console.log(err)
   })
+  if (valid) {
+    const formData = await getFormData()
+    console.log(formData)
+    Object.assign(data, formData)
+    dialogVisible2.value = false
+  }
 }
 </script>
 
 <template>
-  <Descriptions
-    :title="t('descriptionsDemo.descriptions')"
-    :message="t('descriptionsDemo.descriptionsDes')"
-    :data="data"
-    :schema="schema"
-  />
-
-  <Form is-custom :model="form" :rules="rules" @register="formRegister">
+  <ContentWrap :title="t('dialogDemo.dialog')" :message="t('dialogDemo.dialogDes')">
     <Descriptions
-      :title="t('descriptionsDemo.form')"
+      :title="t('descriptionsDemo.descriptions')"
+      :message="t('descriptionsDemo.descriptionsDes')"
       :data="data"
-      :schema="schema2"
-      class="mt-20px"
+      :schema="schema"
     />
     <div class="text-center mt-10px">
-      <BaseButton @click="formValidation"> {{ t('formDemo.formValidation') }} </BaseButton>
+      <BaseButton type="primary" @click="dialogVisible2 = !dialogVisible2">
+        {{ t('exampleDemo.edit') }}
+      </BaseButton>
     </div>
-  </Form>
+
+    <Dialog v-model="dialogVisible2" :title="t('dialogDemo.dialog')">
+      <Form :schema="schema2" :rules="rules" @register="formRegister" />
+      <template #footer>
+        <BaseButton type="primary" @click="formSubmit">{{ t('dialogDemo.submit') }}</BaseButton>
+        <BaseButton @click="dialogVisible2 = false">{{ t('dialogDemo.close') }}</BaseButton>
+      </template>
+    </Dialog>
+  </ContentWrap>
 </template>
 
 <style lang="less" scoped>
